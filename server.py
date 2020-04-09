f865fcc5dccb851b2a32b4108869856f463ca889
bytewzk <2462525363@qq.com>
2020-04-09 14:05:55 +0800

    Implemented Ranklist & Submission List
    Excited!

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import flask, os, uuid, time, threading
+import flask, os, uuid, time, threading, pickle
 
 TIME_LIMIT = 120
 
@@ -7,8 +7,17 @@ mutex = threading.Lock()
 task_queue = []
 result_set = {}
 user_task_list = {}
+best_score = {}
 
-
+def save_data():
+	pickle.dump(best_score,open("best_score.txt","wb"))
+	pickle.dump(user_task_list,open("user_task_list.txt","wb"))
+	pickle.dump(result_set,open("result_set.txt","wb"))
+def load_data():
+	global best_score,user_task_list,result_set
+	best_score = pickle.load(open("best_score.txt","rb"))
+	user_task_list = pickle.load(open("user_task_list.txt","rb"))
+	result_set = pickle.load(open("result_set.txt","rb"))
 @app.route("/add_task", methods= ["POST"])
 def add_task():
 	# check file exist
@@ -23,11 +32,12 @@ def add_task():
 	# get uuid and save file
 	task_id = str(uuid.uuid1())
 	file.save("pending_files/" + task_id)
-	result_set[task_id] = {"status": "pending", "time": None,"detail":"","task_id":task_id}
+	result_set[task_id] = {"status": "Pending","submit_time":time.ctime(),"cmptime":time.time(), "time": None,"detail":"","task_id":task_id,"username" : username}
 
 	# attach task to user
 	if username not in user_task_list:
 		user_task_list[username] = []
+		best_score[username] = 1e9
 	user_task_list[username].append(task_id)
 
 	# add to queue
@@ -41,7 +51,16 @@ def add_task():
 def submit():
 	no_file = flask.request.args.get("no_file") == '1' or False
 	no_username = flask.request.args.get("no_username") == '1' or False
-	return flask.render_template("submit.html", no_file=no_file, no_username=no_username)
+	current_bst = []
+	for i in best_score:
+		current_bst.append((best_score[i],i))
+	current_bst = sorted(current_bst)
+	while len(current_bst)!=0 and current_bst[0][0] == None and current_bst[-1][0] != None:
+		current_bst.append(current_bst[0])
+		del current_bst[0]
+	submissions = sorted([result_set[i] for i in result_set],key = lambda x:x["cmptime"])
+	submissions.reverse()
+	return flask.render_template("submit.html", no_file=no_file, no_username=no_username,ranklist = current_bst,submissions = submissions)
 
 @app.route("/user/<username>", methods = ["GET"])
 def user_history(username):
@@ -57,6 +76,7 @@ def user_history(username):
 # TODO
 def init():
 	import os
+	load_data()
 	os.system("mkdir -p pending_files")
 
 
@@ -65,17 +85,17 @@ def judge(task_id):
 	os.system("mv pending_files/{} /main.cpp".format(task_id))
 	os.system("./judge.sh")
 
-	with open("/compile_result.txt", "rb") as f:
+	with open("/compile_result.txt", "r") as f:
 		result = f.read(1)
 		if result:
-			return (None,"Compile Error", open("/compile_result.txt", "r").read())
+			return (1e9,"Compile Error", open("/compile_result.txt", "r").read())
 
 	with open("/time_result.txt", "r") as f:
 		result = f.read()
 		if not result:
-			return (None,"Time Limit Exceeded", "")
+			return (1e9,"Time Limit Exceeded", "")
 		if "Segmentation fault" in result:
-			return (None,"Run Time Error", "")
+			return (1e9,"Run Time Error", "")
 
 		detail = result
 		time_used = float(result.split(" seconds time elapsed")[0].split(" ")[-1])
@@ -83,12 +103,12 @@ def judge(task_id):
 	with open("/output_check.txt", "rb") as f:
 		result = f.read(1)
 		if result:
-			return (None,"No Output", detail)
+			return (1e9,"No Output", detail)
 
 	with open("/answer_result.txt", "rb") as f:
 		result = f.read(1)
 		if result:
-			return (None,"Wrong Answer", detail)
+			return (1e9,"Wrong Answer", detail)
 
 	return (time_used, "Accepted", detail)
 
@@ -108,13 +128,14 @@ def judger():
 		# run task
 		result_set[task_id]["status"] = "Running"
 		result = judge(task_id)
+		cu = result_set[task_id]["username"]
+		best_score[cu] = min(result[0],best_score[cu])
 		result_set[task_id]["time"] = result[0]
 		result_set[task_id]["status"] = result[1]
 		result_set[task_id]["detail"] = result[2]
-
+		save_data()
 if __name__ == "__main__":
 	init()
 	judger_thread = threading.Thread(target=judger)
 	judger_thread.start()
-	app.debug = True
-	app.run(host='0.0.0.0', port=5001)
+	app.run(host='0.0.0.0', port=5001,debug = True)
